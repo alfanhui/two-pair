c114f26640013b45f48e85ce835af5a7de8b08e0
Stu Huddy <program.foo@gmail.com>
2017-05-23 13:03:39 +0100

    Initial Commit
    Initial Commit - Adding Unity Files

---
+++ Assets/Scripts/AnimationPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationPlayer : MonoBehaviour {
+
+    public void PlayAnimation(string animationName) {
+        GetComponent<Animator>().Play(animationName, 0, 0f);
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+
+
+	
+}
